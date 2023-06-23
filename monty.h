576331fc42b84ed908deb0e1ffd354a64d70bade
Amilemia <aminemadah2@gmail.com>
2023-06-23 03:34:49 +0100

    Init

--- monty.h
+++ monty.h
@@ -1,13 +1,13 @@
 #ifndef MONTY_H
 #define MONTY_H
 
-#include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <fcntl.h>
+#include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include <unistd.h>
+
+typedef unsigned int uint;
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -16,7 +16,7 @@
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO Holberton project
+ * for stack, queues, LIFO, FIFO
  */
 
 typedef struct stack_s
@@ -27,61 +27,90 @@ typedef struct stack_s
 } stack_t;
 
 /**
- * struct bus_s - variables -args, file, line content
- * @arg: value
- * @file: pointer to monty file
- * @content: line content
- * @lifi: flag change stack <-> queue
- * Description: carries values through the program
- */
-
-typedef struct bus_s
-{
-	char *arg;
-	FILE *file;
-	char *content;
-	int lifi;
-}  bus_t;
-extern bus_t bus;
-
-/**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO Holberton project
+ * for stack, queues, LIFO, FIFO
  */
 
 typedef struct instruction_s
 {
 	char *opcode;
-	void (*f)(stack_t **stack, unsigned int line_number);
+	void (*f)(stack_t **stack, uint line_number);
 } instruction_t;
 
-char *_realloc(char *ptr, unsigned int old_size, unsigned int new_size);
-ssize_t getstdin(char **lineptr, int file);
-char  *clean_line(char *content);
-void f_push(stack_t **head, unsigned int number);
-void f_pall(stack_t **head, unsigned int number);
-void f_pint(stack_t **head, unsigned int number);
-int execute(char *content, stack_t **head, unsigned int counter, FILE *file);
-void free_stack(stack_t *head);
-void f_pop(stack_t **head, unsigned int counter);
-void f_swap(stack_t **head, unsigned int counter);
-void f_add(stack_t **head, unsigned int counter);
-void f_nop(stack_t **head, unsigned int counter);
-void f_sub(stack_t **head, unsigned int counter);
-void f_div(stack_t **head, unsigned int counter);
-void f_mul(stack_t **head, unsigned int counter);
-void f_mod(stack_t **head, unsigned int counter);
-void f_pchar(stack_t **head, unsigned int counter);
-void f_pstr(stack_t **head, unsigned int counter);
-void f_rotl(stack_t **head, unsigned int counter);
-void f_rotr(stack_t **head, __attribute__((unused)) unsigned int counter);
-void addnode(stack_t **head, int n);
-void addqueue(stack_t **head, int n);
-void f_queue(stack_t **head, unsigned int counter);
-void f_stack(stack_t **head, unsigned int counter);
+/**
+ * enum format_e - Format of the data
+ * @STACK: Stack (LIFO) format
+ * @QUEUE: Queue (FIFO) format
+ *
+ * Format of the data for the Monty bytecode interpreter
+ */
+
+typedef enum format_e
+{
+	STACK,
+	QUEUE
+} format_t;
+
+/**
+ * struct global_s - global variables for Monty interpreter
+ * @fp: file pointer to Monty bytecode file
+ * @line: line buffer for getline
+ * @len: line length for getline
+ * @nread: number of characters read by getline
+ * @line_number: current line number in Monty bytecode file
+ * @stack: pointer to top of stack
+ * @format: Format of the data (stack or queue)
+ *
+ * Description: global variables for Monty interpreter
+ */
+
+typedef struct global_s
+{
+	FILE *fp;
+	char *line;
+	size_t len;
+	ssize_t nread;
+	uint line_number;
+	stack_t *stack;
+	format_t format;
+} global_t;
+
+extern global_t data;
+
+/* op_1.c */
+void push(stack_t **stack, uint line_number);
+void pall(stack_t **stack, uint line_number);
+void pint(stack_t **stack, uint line_number);
+void pop(stack_t **stack, uint line_number);
+void add(stack_t **stack, uint line_number);
+/* op_2.c */
+void nop(stack_t **stack, uint line_number);
+void sub(stack_t **stack, uint line_number);
+void _div(stack_t **stack, uint line_number);
+void mul(stack_t **stack, uint line_number);
+void mod(stack_t **stack, uint line_number);
+/* op_3.c */
+void swap(stack_t **stack, uint line_number);
+void pchar(stack_t **stack, uint line_number);
+void pstr(stack_t **stack, uint line_number);
+void rotl(stack_t **stack, uint line_number);
+void rotr(stack_t **stack, uint line_number);
+/* op_mode.c */
+void stack(stack_t **stack, uint line_number);
+void queue(stack_t **stack, uint line_number);
+
+/* utils.c */
+int is_number(char *str);
+void (*get_op_func(char *opcode))(stack_t **stack, uint line_number);
+void parse_line(void);
+
+/* utils_node.c */
+stack_t *add_node(stack_t **head, const int n);
+void free_stack(stack_t **head);
+stack_t *add_node_end(stack_t **head, const int n);
 
 #endif
